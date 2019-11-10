b7607c933357102719164b4ffdc3d92a9ef118f2
Isaak Hanimann <isaak@Isaaks-MacBook-Pro.local>
2019-11-10 11:32:33 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:float/screens/welcome_screen.dart';
+import 'package:float/screens/login_screen.dart';
+import 'package:float/screens/registration_screen.dart';
+import 'package:float/screens/chat_screen.dart';
+
+void main() => runApp(Float());
+
+class Float extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
