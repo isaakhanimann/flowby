ca6de9e77958b175522511f6482966d092ac7588
isaaknewton <isaak.hanimann@gmail.com>
2019-12-25 12:58:12 +0100

    Move search functionality to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:float/constants.dart';
 import 'package:float/models/user.dart';
 import 'package:float/services/firebase_connection.dart';
 import 'package:float/widgets/stream_list_users.dart';
+import 'package:float/widgets/users_listview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -16,11 +18,19 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var loggedInUser = Provider.of<FirebaseUser>(context);
 
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
+    //to do the search call:
+//    showSearch(
+//        context: context,
+//        delegate: DataSearch(isSkillSearch: isSkillSelected));
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
           children: <Widget>[
             CupertinoSegmentedControl(
+              borderColor: kDarkGreenColor,
+              pressedColor: kLightGreenColor,
+              selectedColor: kDarkGreenColor,
               groupValue: isSkillSelected,
               onValueChanged: switchSearch,
               children: <bool, Widget>{
@@ -53,6 +63,162 @@ class HomeScreen extends StatelessWidget {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+//Search Functionality
+
+class DataSearch extends SearchDelegate<String> {
+  bool isSkillSearch;
+  DataSearch({@required this.isSkillSearch});
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.menu_arrow,
+        progress: transitionAnimation,
+      ),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    var loggedInUser = Provider.of<FirebaseUser>(context);
+    return StreamBuilder(
+        stream: FirebaseConnection.getUserStream(uid: loggedInUser.email),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Container(color: Colors.white);
+          }
+          User user = snapshot.data;
+          return StreamBuilder<List<User>>(
+            stream: FirebaseConnection.getUsersStreamWithDistance(
+                loggedInUser: user),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: Text('Nodata'),
+                );
+              }
+
+              final List<User> allUsers = snapshot.data;
+              List<User> suggestedUsers;
+
+              if (isSkillSearch) {
+                suggestedUsers = allUsers
+                    .where((u) => u.skillHashtags
+                        .toString()
+                        .toLowerCase()
+                        .contains(query.toLowerCase()))
+                    .toList();
+              } else {
+                suggestedUsers = allUsers
+                    .where((u) => u.wishHashtags
+                        .toString()
+                        .toLowerCase()
+                        .contains(query.toLowerCase()))
+                    .toList();
+              }
+              return UsersListView(
+                  users: suggestedUsers, searchSkill: isSkillSearch);
+            },
+          );
+        });
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    var loggedInUser = Provider.of<FirebaseUser>(context);
+
+    return StreamBuilder<List<User>>(
+      stream: FirebaseConnection.getUsersStream(loggedInUser: loggedInUser),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: Text('Nodata'),
+          );
+        }
+
+        final List<User> allUsers = snapshot.data;
+
+        List<User> suggestedUsers;
+
+        if (isSkillSearch) {
+          suggestedUsers = allUsers
+              .where((u) => u.skillHashtags
+                  .toString()
+                  .toLowerCase()
+                  .contains(query.toLowerCase()))
+              .toList();
+        } else {
+          suggestedUsers = allUsers
+              .where((u) => u.wishHashtags
+                  .toString()
+                  .toLowerCase()
+                  .contains(query.toLowerCase()))
+              .toList();
+        }
+
+        return ListView.builder(
+          itemCount: suggestedUsers.length,
+          itemExtent: 50,
+          itemBuilder: (context, index) {
+            return SuggestionItem(
+              user: suggestedUsers[index],
+              setQuery: (newQuery) {
+                query = newQuery;
+              },
+              showResults: showResults,
+              isSkillSearch: isSkillSearch,
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class SuggestionItem extends StatelessWidget {
+  final User user;
+  final Function setQuery;
+  final Function showResults;
+  final bool isSkillSearch;
+
+  SuggestionItem(
+      {@required this.user,
+      @required this.setQuery,
+      @required this.showResults,
+      @required this.isSkillSearch});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        setQuery(isSkillSearch ? user.skillHashtags : user.wishHashtags);
+        showResults(context);
+      },
+      leading: Icon(Icons.insert_emoticon),
+      title: Text(
+        isSkillSearch ? user.skillHashtags : user.wishHashtags,
+        style: TextStyle(fontSize: 18),
       ),
     );
   }
--- lib/screens/navigation_screen.dart
+++ lib/screens/navigation_screen.dart
@@ -1,13 +1,9 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:float/models/user.dart';
+import 'package:float/constants.dart';
 import 'package:float/screens/chat_overview_screen.dart';
 import 'package:float/screens/create_profile_screen.dart';
 import 'package:float/screens/home_screen.dart';
-import 'package:float/services/firebase_connection.dart';
-import 'package:float/widgets/users_listview.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class NavigationScreen extends StatefulWidget {
   static const String id = 'navigation_screen';
@@ -28,18 +24,13 @@ class _NavigationScreenState extends State<NavigationScreen> {
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
       tabBar: CupertinoTabBar(
+        activeColor: kDarkGreenColor,
         items: [
           BottomNavigationBarItem(
               icon: Icon(
                 CupertinoIcons.home,
               ),
               title: Text('Home')),
-          BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.search,
-            ),
-            title: Text('Search'),
-          ),
           BottomNavigationBarItem(
             icon: Icon(
               CupertinoIcons.conversation_bubble,
@@ -62,23 +53,15 @@ class _NavigationScreenState extends State<NavigationScreen> {
                   isSkillSelected: isSkillSelected, switchSearch: switchSearch);
               break;
             case 1:
-              //search was pressed
-              showSearch(
-                  context: context,
-                  delegate: DataSearch(isSkillSearch: isSkillSelected));
-              return HomeScreen(
-                  isSkillSelected: isSkillSelected, switchSearch: switchSearch);
-              break;
-            case 2:
               return ChatOverviewScreen();
               break;
-            case 3:
+            case 2:
               return CreateProfileScreen();
               break;
           }
           return ChatOverviewScreen();
         });
-
+//to navigate somewhere else do:
 //        return CupertinoTabView(
 //          builder: (BuildContext context) {
 //            return CupertinoPageScaffold(
@@ -119,37 +102,6 @@ class _NavigationScreenState extends State<NavigationScreen> {
   }
 }
 
-//class NavigationScreens extends StatefulWidget {
-//  static const String id = 'navigation_screen';
-//
-//  @override
-//  _NavigationScreensState createState() => _NavigationScreensState();
-//}
-//
-//class _NavigationScreensState extends State<NavigationScreens> {
-//  int _selectedPage = 0;
-//  bool showSearchBar = false;
-//  List<Widget> _pageOptions;
-//  bool isSkillSelected = true;
-//
-//  void switchSearch(int index) {
-//    setState(() {
-//      isSkillSelected = !isSkillSelected;
-//    });
-//  }
-//
-//  Widget _getPage({int pageNumber}) {
-//    switch (pageNumber) {
-//      case 0:
-//        return _pageOptions[0];
-//      case 1:
-//        return _pageOptions[0];
-//      case 2:
-//        return _pageOptions[1];
-//      default:
-//        return _pageOptions[2];
-//    }
-//  }
 //
 //  @override
 //  void didChangeDependencies() {
@@ -160,207 +112,3 @@ class _NavigationScreenState extends State<NavigationScreen> {
 //    }
 //  }
 //
-//  @override
-//  Widget build(BuildContext context) {
-//    _pageOptions = [
-//      HomeScreen(
-//          selections: [isSkillSelected, !isSkillSelected],
-//          switchSearch: switchSearch),
-//      ChatOverviewScreen(),
-//      CreateProfileScreen()
-//    ];
-//    return Scaffold(
-//      body: SafeArea(child: _getPage(pageNumber: _selectedPage)),
-//      bottomNavigationBar: CupertinoTabBar(
-//        activeColor: kDarkGreenColor,
-//        currentIndex: _selectedPage,
-//        onTap: (int index) async {
-//          switch (index) {
-//            case 0:
-//              setState(() {
-//                _selectedPage = 0;
-//              });
-//              break;
-//            case 1:
-//              //search was pressed
-//              await showSearch(
-//                  context: context,
-//                  delegate: DataSearch(isSkillSearch: isSkillSelected));
-//              setState(() {
-//                _selectedPage = 1;
-//              });
-//              break;
-//            case 2:
-//              setState(() {
-//                _selectedPage = 2;
-//              });
-//              break;
-//            case 3:
-//              setState(() {
-//                _selectedPage = 3;
-//              });
-//              break;
-//            default:
-//              setState(() {
-//                _selectedPage = 0;
-//              });
-//          }
-//        },
-//      ),
-//    );
-//  }
-//}
-
-class DataSearch extends SearchDelegate<String> {
-  bool isSkillSearch;
-  DataSearch({@required this.isSkillSearch});
-
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      )
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: AnimatedIcon(
-        icon: AnimatedIcons.menu_arrow,
-        progress: transitionAnimation,
-      ),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    var loggedInUser = Provider.of<FirebaseUser>(context);
-    return StreamBuilder(
-        stream: FirebaseConnection.getUserStream(uid: loggedInUser.email),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return Container(color: Colors.white);
-          }
-          User user = snapshot.data;
-          return StreamBuilder<List<User>>(
-            stream: FirebaseConnection.getUsersStreamWithDistance(
-                loggedInUser: user),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return Center(
-                  child: Text('Nodata'),
-                );
-              }
-
-              final List<User> allUsers = snapshot.data;
-              List<User> suggestedUsers;
-
-              if (isSkillSearch) {
-                suggestedUsers = allUsers
-                    .where((u) => u.skillHashtags
-                        .toString()
-                        .toLowerCase()
-                        .contains(query.toLowerCase()))
-                    .toList();
-              } else {
-                suggestedUsers = allUsers
-                    .where((u) => u.wishHashtags
-                        .toString()
-                        .toLowerCase()
-                        .contains(query.toLowerCase()))
-                    .toList();
-              }
-              return UsersListView(
-                  users: suggestedUsers, searchSkill: isSkillSearch);
-            },
-          );
-        });
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    var loggedInUser = Provider.of<FirebaseUser>(context);
-
-    return StreamBuilder<List<User>>(
-      stream: FirebaseConnection.getUsersStream(loggedInUser: loggedInUser),
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Center(
-            child: Text('Nodata'),
-          );
-        }
-
-        final List<User> allUsers = snapshot.data;
-
-        List<User> suggestedUsers;
-
-        if (isSkillSearch) {
-          suggestedUsers = allUsers
-              .where((u) => u.skillHashtags
-                  .toString()
-                  .toLowerCase()
-                  .contains(query.toLowerCase()))
-              .toList();
-        } else {
-          suggestedUsers = allUsers
-              .where((u) => u.wishHashtags
-                  .toString()
-                  .toLowerCase()
-                  .contains(query.toLowerCase()))
-              .toList();
-        }
-
-        return ListView.builder(
-          itemCount: suggestedUsers.length,
-          itemExtent: 50,
-          itemBuilder: (context, index) {
-            return SuggestionItem(
-              user: suggestedUsers[index],
-              setQuery: (newQuery) {
-                query = newQuery;
-              },
-              showResults: showResults,
-              isSkillSearch: isSkillSearch,
-            );
-          },
-        );
-      },
-    );
-  }
-}
-
-class SuggestionItem extends StatelessWidget {
-  final User user;
-  final Function setQuery;
-  final Function showResults;
-  final bool isSkillSearch;
-
-  SuggestionItem(
-      {@required this.user,
-      @required this.setQuery,
-      @required this.showResults,
-      @required this.isSkillSearch});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        setQuery(isSkillSearch ? user.skillHashtags : user.wishHashtags);
-        showResults(context);
-      },
-      leading: Icon(Icons.insert_emoticon),
-      title: Text(
-        isSkillSearch ? user.skillHashtags : user.wishHashtags,
-        style: TextStyle(fontSize: 18),
-      ),
-    );
-  }
-}
