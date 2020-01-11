0a4b6fa8f224ec3fd9391369b6fbfffa25e5bdd8
isaaknewton <isaak.hanimann@gmail.com>
2020-01-11 18:25:42 +0100

    Add editprofilescreen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:float/constants.dart';
 import 'package:float/screens/choose_signup_or_login_screen.dart';
-import 'package:float/screens/create_profile_screen.dart';
+import 'package:float/screens/edit_profile_screen.dart';
 import 'package:float/services/firebase_auth_service.dart';
 import 'package:float/widgets/rounded_button.dart';
 import 'package:flutter/cupertino.dart';
@@ -51,6 +51,7 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 leading: CircleAvatar(
                   radius: 20,
                   backgroundColor: Colors.grey,
+                  // todo get the real imagefilename of the logged in user
                   backgroundImage: NetworkImage(
                       'https://firebasestorage.googleapis.com/v0/b/float-a5628.appspot.com/o/images%2F${'default-profile-pic.jpg'}?alt=media'),
                 ),
@@ -59,7 +60,9 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   Navigator.of(context, rootNavigator: true).push(
                     CupertinoPageRoute<void>(
                       builder: (context) {
-                        return CreateProfileScreen();
+                        return EditProfileScreen(
+                          loggedInUser: loggedInUser,
+                        );
                       },
                     ),
                   );
