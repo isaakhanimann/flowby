669979253e9288f4279a33d593800d26056ba44b
Alexandre Roque <hiwz2ster@gmail.com>
2020-03-30 12:09:20 +0200

    when user quits a chat update the number of unread messages

--- lib/models/chat.dart
+++ lib/models/chat.dart
@@ -5,36 +5,36 @@ class Chat {
   List<String> combinedUids;
   User user1;
   bool hasUser1Blocked;
+  int unreadMessages1;
   User user2;
   bool hasUser2Blocked;
+  int unreadMessages2;
   String chatpath;
   String lastMessageText;
   var lastMessageTimestamp;
-  int unreadMessages1;
-  int unreadMessages2;
 
   Chat(
       {this.combinedUids,
       this.user1,
       this.hasUser1Blocked,
+      this.unreadMessages1 = 0,
       this.user2,
       this.hasUser2Blocked,
+      this.unreadMessages2 = 0,
       this.lastMessageText,
-      this.lastMessageTimestamp,
-      this.unreadMessages1 = 0,
-      this.unreadMessages2 = 0});
+      this.lastMessageTimestamp});
 
   Chat.fromMap({Map<String, dynamic> map}) {
     this.combinedUids = _convertFirebaseList(list: map['combinedUids']);
     this.user1 = User.fromMap(map: map['user1']);
     this.hasUser1Blocked = map['hasUser1Blocked'] ?? false;
+    this.unreadMessages1 = map['unreadMessages1'] ?? 0;
     this.user2 = User.fromMap(map: map['user2']);
     this.hasUser2Blocked = map['hasUser2Blocked'] ?? false;
+    this.unreadMessages2 = map['unreadMessages2'] ?? 0;
     this.lastMessageText = map['lastMessageText'] ?? 'No messages';
     this.lastMessageTimestamp =
         map['lastMessageTimestamp']?.toDate() ?? DateTime.now();
-    this.unreadMessages1 = map['unreadMessages1'] ?? 0;
-    this.unreadMessages2 = map['unreadMessages2'] ?? 0;
   }
 
   Map<String, dynamic> toMap() {
@@ -42,12 +42,12 @@ class Chat {
       'combinedUids': combinedUids,
       'user1': user1.toMap(),
       'hasUser1Blocked': hasUser1Blocked,
+      'unreadMessages1': unreadMessages1,
       'user2': user2.toMap(),
       'hasUser2Blocked': hasUser2Blocked,
+      'unreadMessages2': unreadMessages2,
       'lastMessageText': lastMessageText,
       'lastMessageTimestamp': lastMessageTimestamp,
-      'unreadMessages1' : unreadMessages1,
-      'unreadMessages2' : unreadMessages2,
     };
   }
 
@@ -60,12 +60,12 @@ class Chat {
     String toPrint = '\n{ user1: $user1, ';
     toPrint += 'combinedUids: $combinedUids';
     toPrint += 'hasUser1Blocked: $hasUser1Blocked, ';
+    toPrint += 'unreadMessages1: $unreadMessages1, ';
     toPrint += 'user2: $user2, ';
     toPrint += 'hasUser2Blocked: $hasUser2Blocked, ';
+    toPrint += 'unreadMessages2: $unreadMessages1, ';
     toPrint += 'lastMessageText: $lastMessageText, ';
-    toPrint += 'lastMessageTimestamp: ${lastMessageTimestamp.toString()} ';
-    toPrint += 'unreadMessages1: $unreadMessages1, ';
-    toPrint += 'unreadMessages2: $unreadMessages1, }\n';
+    toPrint += 'lastMessageTimestamp: ${lastMessageTimestamp.toString()} }\n';
     return toPrint;
   }
 
